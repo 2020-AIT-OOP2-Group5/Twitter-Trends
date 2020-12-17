89e668e85d1a3ff9f72fe5d7daf69b6c02b5858a
k19116 <75160594+k19116@users.noreply.github.com>
2020-12-17 13:53:47 +0900

    Web flask
    main.pyにFlask追加

---
+++ main.py
@@ -0,0 +1,66 @@
+from pytrends.request import TrendReq
+from flask import Flask, request, render_template, redirect
+import pandas, numpy
+from twitter import *
+import re
+import tweepy
+
+pytrends = TrendReq()
+trend = pytrends.trending_searches(pn = 'japan')
+
+CK = "6c3ueDtot5JInpVt11Ji2c7wh"
+CS = "8Yw06209wso7ykMK6rEET461QDUXEKhtF6EQ7rVqQRnc1TWqeH"
+AT = "1336913672803733504-hQgzG4BiShULaBoQ8Meno3YQJjltC7"
+AS = "ahbjvKy9mYn0lvlAdJUjmp0A2cetfpeXWMFA3iW6UNOlT"
+
+twitter = Twitter(auth = OAuth(AT,AS,CK,CS))
+results = twitter.trends.place(_id = 23424856)
+
+Pre_Twitter_list = []
+Tweet_Twitter_list = []
+for location in results:
+        for trend in location["trends"]:
+            # if trend["tweet_volume"] != None:            
+            Pre_Twitter_list.append(trend["name"])
+            Tweet_Twitter_list.append(trend["tweet_volume"])
+
+twitter_list = ' '.join(Pre_Twitter_list[:5])
+tweet_list = ' '.join(map(str,Tweet_Twitter_list[:5]))
+print(twitter_list)
+print(tweet_list)
+
+twitter_list = twitter_list.split(" ")
+tweet_list = tweet_list.split(" ")
+
+
+
+auth = tweepy.OAuthHandler(CK, CS)
+auth.set_access_token(AT, AS)
+api = tweepy.API(auth)
+
+top_tweet = []
+
+for i in range(5):
+    tweets = api.search(q=twitter_list[i], count=1, tweet_mode='extended')
+    for tweet in tweets:
+        top_tweet.append([tweet.retweet_count,tweet.full_text.replace('\n','')])
+
+print(top_tweet)
+
+
+#ここからweb
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', tweet_list=tweet_list)
+
+@app.route('/kousin')
+def koushin():
+
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
