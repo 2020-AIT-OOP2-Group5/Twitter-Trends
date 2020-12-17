af4174ea4367735efa95763ca18c20765f8aa121
Nonaka000 <75057528+Nonaka000@users.noreply.github.com>
2020-12-17 14:18:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -57,10 +57,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', tweet_list=tweet_list)
 
-@app.route('/kousin')
-def koushin():
-
-    return redirect("/")
-
 if __name__ == "__main__":
     app.run(debug=True)
