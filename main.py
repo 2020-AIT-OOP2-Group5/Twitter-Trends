83ce749209b2de47b9a9c5169db25d0f2d3c6114
k19116 <75160594+k19116@users.noreply.github.com>
2020-12-19 14:26:34 +0900

    web更新#6

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pytrends.request import TrendReq
-import pandas, numpy
+import pandas
+import numpy
 from twitter import *
 import re
 import tweepy
@@ -8,69 +9,77 @@ import datetime
 import json
 
 pytrends = TrendReq()
-trend = pytrends.trending_searches(pn = 'japan')
+trend = pytrends.trending_searches(pn='japan')
 
-json_open = open('Twitter_Key&Token.json','r')
+json_open = open('Twitter_Key&Token.json', 'r')
 twitter_key_token = json.load(json_open)
 
 print(twitter_key_token['AT'])
 
-twitter = Twitter(auth = OAuth(twitter_key_token['AT'],twitter_key_token['AS'],twitter_key_token['CK'],twitter_key_token['CS']))
-results = twitter.trends.place(_id = 23424856)
+twitter = Twitter(auth=OAuth(
+    twitter_key_token['AT'], twitter_key_token['AS'], twitter_key_token['CK'], twitter_key_token['CS']))
+results = twitter.trends.place(_id=23424856)
 
 Pre_Twitter_list = []
 Tweet_Twitter_list = []
 for location in results:
-        for trend in location["trends"]:
-            if trend["tweet_volume"] != None:            
-                Pre_Twitter_list.append(trend["name"])
-                Tweet_Twitter_list.append(trend["tweet_volume"])
+    for trend in location["trends"]:
+        if trend["tweet_volume"] != None:
+            Pre_Twitter_list.append(trend["name"])
+            Tweet_Twitter_list.append(trend["tweet_volume"])
 
 twitter_list = ' '.join(Pre_Twitter_list[:5])
-tweet_list = ' '.join(map(str,Tweet_Twitter_list[:5]))
+tweet_list = ' '.join(map(str, Tweet_Twitter_list[:5]))
 print(twitter_list)
 print(tweet_list)
 
 twitter_list = twitter_list.split(" ")
 tweet_list = tweet_list.split(" ")
 
-auth = tweepy.OAuthHandler(twitter_key_token['CK'],twitter_key_token['CS'] )
+auth = tweepy.OAuthHandler(twitter_key_token['CK'], twitter_key_token['CS'])
 auth.set_access_token(twitter_key_token['AT'], twitter_key_token['AS'])
 api = tweepy.API(auth)
 
-top_tweet = ["","","","",""]
+top_tweet = ["", "", "", "", ""]
 tweet_text = ""
 max_retweet = 0
 
 for i in range(5):
-    
-    tweets = api.search(q=twitter_list[i], count=tweet_list[i], tweet_mode='extended')
+
+    tweets = api.search(
+        q=twitter_list[i], count=tweet_list[i], tweet_mode='extended')
     for tweet in tweets:
         if max_retweet < tweet.retweet_count:
-            tweet_text = tweet.full_text.replace('\n','')
+            tweet_text = tweet.full_text.replace('\n', '')
             max_retweet = tweet.retweet_count
 
     # top_tweet.append(tweet_text)
     top_tweet[i] = tweet_text
-    top_tweet[i] = top_tweet[i].split(':',1)[1]
+    top_tweet[i] = top_tweet[i].split(':', 1)[1]
     max_retweet = 0
     print(top_tweet[i])
-    
-
 
 
 time = datetime.datetime.now()
 
 print(time.strftime('%Y年%m月%d日 %H:%M:%S'))
 
-#ここからweb
+# ここからweb
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', tweet_list=tweet_list)
+    return render_template('index.html', time=time, twitter_list=twitter_list, tweet_list=tweet_list, top_tweet=top_tweet)
+
+
+@app.route('/koushin')
+def koushin():
+    #　とりあえず
+    #　あとで更新するコードを追加します
+    return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
